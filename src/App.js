2814b5052f6db201aed01ddf20fdc5db2d7c0a55
chentiancheng <1934024095@qq.com>
2018-10-21 19:27:17 +0800

    react——demo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route,Link} from "react-router-dom";
+import "../node_modules/antd/dist/antd.css";
+import {antd} from "antd";
+import SetingHome from "./page/SetingHome";
+import Contents from "./page/content";
+import User from "./page/user";
+import Login from "./page/Login";
+import Email from "./page/email";
+import Move from "./page/move";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="SetingNavbar">
+      <Router>    
+          <div>
+              <Route  exact path={"/"} component={Login}  />
+              <Route path={"/SetingHome"} component={SetingHome}  />
+              <Route path={"/content"} component={Contents}  />
+              <Route path={"/user"} component={User}  />
+              <Route path={"/SetingHome/email"} component={Email}  />
+              <Route path={"/SetingHome/move"} component={Move}  />
+          </div>
+      </Router>
       </div>
     );
   }
 }
-
 export default App;
